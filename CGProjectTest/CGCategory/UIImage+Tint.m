3366e34faea4a760071049914dd348f342afb02c
LeoCai <leocai@LeoCais-MacBook-Pro.local>
2017-01-14 11:48:09 +0800

    change:给image的Tint实现部分增加注释。

--- CGProjectTest/CGCategory/UIImage+Tint.m
+++ CGProjectTest/CGCategory/UIImage+Tint.m
@@ -24,18 +24,22 @@
 
 - (UIImage *)imageWithTintColor:(UIColor *)tintColor blendMode:(CGBlendMode)blendMode
 {
+    // 开启image上下文
     UIGraphicsBeginImageContextWithOptions(self.size, NO, 0);
     
+    // 渲染设置
     [tintColor setFill];
     
     CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
     UIRectFill(bounds);
     
+    // 设置blendMode绘制
     [self drawInRect:bounds blendMode:blendMode alpha:1.f];
     if (blendMode != kCGBlendModeDestinationIn) {
         [self drawInRect:bounds blendMode:kCGBlendModeDestinationIn alpha:1.f];
     }
     
+    // 获取新的Image
     UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     
